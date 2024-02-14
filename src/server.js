920d46cedb2c63f58e9888ae3ad699ba0b91909b
Treaders49 <lat1404@hotmail.co.uk>
2024-02-14 17:25:06 +0000

    began developing new account endpoint

--- src/server.js
+++ src/server.js
@@ -1,8 +1,33 @@
 import express from "express";
+import bcrypt from "bcrypt"
 const app = express();
+import bodyParser from "body-parser"
+import {MongoClient, ServerApiVersion} from 'mongodb';
+import cors from "cors";
+const uri = "mongodb+srv://root:pass@ptcluster.9ktwtrt.mongodb.net/?retryWrites=true&w=majority";
+
+app.use(cors());
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
 app.use(express.json());
-app.post('/hello', (req, res) => {
-    res.send(`hello! ${req.body.name}`);
+
+
+app.post('/newAccount',async (req, res) => {
+    const {fullname, username, email, dob, password} = req.body;
+    const hashPass = await bcrypt.hash(password, salt)
+    console.log(hashPass);
+    await client.connect();
+    const db = client.db("TrainingAppDB");
+    await db.collection("Users").insertOne({"fullname": fullname, "username": username, "email": email, "dob": dob, "password": hashPass})
+
+    
 });
 
 app.listen(8000, () => {
