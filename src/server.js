447da2a6cc6b0f42b606f338af917b49f7a8a40c
Treaders49 <lat1404@hotmail.co.uk>
2024-02-21 11:36:25 +0000

    successfully adding user details to database with hash

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = express();
 import bodyParser from "body-parser"
 import {MongoClient, ServerApiVersion} from 'mongodb';
 import cors from "cors";
-const uri = "mongodb+srv://root:pass@ptcluster.9ktwtrt.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://lat1404:49Brading@ptcluster.9ktwtrt.mongodb.net/?retryWrites=true&w=majority";
 
 app.use(cors());
 
@@ -20,12 +20,22 @@ app.use(express.json());
 
 
 app.post('/newAccount',async (req, res) => {
-    const {fullname, username, email, dob, password} = req.body;
-    const hashPass = await bcrypt.hash(password, salt)
+    let salt = "";
+    let hashPass = "";
+    const {fullname, username, email, dob, password, selectedExperience, selectedDiscipline} = req.body;
+    bcrypt.genSalt(10).then(newSalt => {
+      salt = newSalt;
+      console.log(salt);
+      return bcrypt.hash(password, salt)
+    }).then(newHashPass => {
+      hashPass = newHashPass;
+    });
     console.log(hashPass);
+    
+    
     await client.connect();
     const db = client.db("TrainingAppDB");
-    await db.collection("Users").insertOne({"fullname": fullname, "username": username, "email": email, "dob": dob, "password": hashPass})
+    await db.collection("Users").insertOne({"fullname": fullname, "username": username, "salt": salt, "email": email, "dob": dob, "password": hashPass})
 
     
 });
